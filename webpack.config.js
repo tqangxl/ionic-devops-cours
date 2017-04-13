9a4f1cd4517937044cdf9631087b2c6c5eb275a0
FazioNico <contact@nicolasfazio.ch>
2017-04-13 07:42:35 +0200

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@
  * @Date:   09-04-2017
  * @Email:  contact@nicolasfazio.ch
  * @Last modified by:   webmaster-fazio
- * @Last modified time: 09-04-2017
+ * @Last modified time: 13-04-2017
  */
 
  var webpack = require('webpack');
  var ionicWebpackFactory = require(process.env.IONIC_WEBPACK_FACTORY);
  var config = require('@ionic/app-scripts/config/webpack.config.js')
 
- config.plugins.push(new webpack.EnvironmentPlugin(['IONIC_ENV']))
+ config.plugins.push(new webpack.EnvironmentPlugin(['IONIC_ENV', 'NODE_ENV']))
 
  module.exports = config
